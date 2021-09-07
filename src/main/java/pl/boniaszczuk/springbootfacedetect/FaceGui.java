ec8ea41b196b76b84d6a71508249f22d4b8109da
Maciej Boniaszczuk <maciek6001@gmail.com>
2021-09-07 20:32:34 +0200

    added refresh button

--- src/main/java/pl/boniaszczuk/springbootfacedetect/FaceGui.java
+++ src/main/java/pl/boniaszczuk/springbootfacedetect/FaceGui.java
@@ -1,16 +1,18 @@
 package pl.boniaszczuk.springbootfacedetect;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import pl.boniaszczuk.springbootfacedetect.model.FaceObject;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+
 
 @Route
 public class FaceGui extends VerticalLayout {
@@ -19,6 +21,7 @@ public class FaceGui extends VerticalLayout {
 
         TextField textField = new TextField("Image");
         Button button = new Button("Show info");
+        Button refreshButton = new Button("Refresh");
 
         List<FaceObject> personList = new ArrayList<>();
 
@@ -33,9 +36,10 @@ public class FaceGui extends VerticalLayout {
             grid.addColumn(c -> c.getFaceAttributes().getSmile()).setHeader("Smile");
             add(new Image(textField.getValue(), textField.getValue()));
         });
+        refreshButton.addClickListener(c -> UI.getCurrent().getPage().reload());
 
 
-        add(textField,button,grid);
+        add(textField,button,grid,refreshButton);
 
 
     }
